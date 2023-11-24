d57c47cad0986bd35b635418977ff7247c5ad2d8
Taser <t171bo@yandex.ru>
2023-11-24 09:38:21 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+  export default{
+    data(){
+      return{
+        name: '',
+        list: [],
+      }
+    },
+    methods:{
+      sayHello(){
+        this.list.push(this.name);
+        console.log(this.list);
+      }
+    }
+  }
+</script>
+
+<template>
+   
+    <h1>Список задач {{ name==''?'_______________':name }}</h1>
+    <p class="dun">Список текущих задач</p>
+    <input class="in" type="text" v-model="name"><br>
+    <button v-if="name==''" disabled>Введите задачу</button>
+    <button v-else @click="sayHello()">Добавить в список</button>
+    <h3>Задача</h3>
+    <ul>
+      <li v-for="item in list">
+      {{ 
+        item
+         }}</li>
+    </ul>
+</template>
+<style scoped>
+
+</style>
